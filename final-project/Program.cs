0c0cedc19739eebe948c49c57e4927bc48c6b040
John Marky Natividad <johnmarky.dev@gmail.com>
2024-11-06 12:06:22 +0800

    Add project files.

---
+++ final-project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace final_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
