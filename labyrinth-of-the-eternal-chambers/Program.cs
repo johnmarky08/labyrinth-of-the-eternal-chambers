2ab4149a763a4abc0546375371cad88713dd16dd
John Marky Natividad <johnmarky.dev@gmail.com>
2024-11-29 16:17:01 +0800

    update

--- labyrinth-of-the-eternal-chambers/Program.cs
+++ labyrinth-of-the-eternal-chambers/Program.cs
@@ -1,15 +1,54 @@
-﻿using WindowsInput;
+﻿using System.Runtime.InteropServices;
+using WindowsInput;
 using WindowsInput.Native;
 
 namespace labyrinth_of_the_eternal_chambers
 {
     internal class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetCurrentConsoleFontEx(IntPtr consoleOutput, bool maximumWindow, ref CONSOLE_FONT_INFO_EX consoleCurrentFontEx);
+
+        private const int STD_OUTPUT_HANDLE = -11;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct CONSOLE_FONT_INFO_EX
+        {
+            public int cbSize;
+            public int nFont;
+            public short dwFontSizeX;
+            public short dwFontSizeY;
+            public int FontFamily;
+            public int FontWeight;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string FaceName;
+        }
+
         /// <summary>
         /// To maximize the console screen and to start the game.
         /// </summary>
         static void Main()
         {
+            IntPtr consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            if (consoleHandle == IntPtr.Zero)
+            {
+                Console.WriteLine("Unable to get console handle.");
+                return;
+            }
+
+            CONSOLE_FONT_INFO_EX fontInfo = new()
+            {
+                cbSize = Marshal.SizeOf<CONSOLE_FONT_INFO_EX>(),
+                FaceName = "Consolas",
+                dwFontSizeX = 0,
+                dwFontSizeY = 12
+            };
+
+            SetCurrentConsoleFontEx(consoleHandle, false, ref fontInfo);
+
             InputSimulator simulator = new();
 
             simulator.Keyboard.KeyPress(VirtualKeyCode.F11);
